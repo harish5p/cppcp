23bdb8bf5f94ae63d75b677587c3ddf1f8a27c99
Tracing Insights <tracing.insights@gmail.com>
2023-09-25 07:51:10 +0000

    99BUS

--- 99BUS.cpp
+++ 99BUS.cpp
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-
-
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
@@ -16,69 +14,65 @@ int main() {
   const int MAX_STOPS = 51;
   const int NEVER = 999999;
 
-  int n; // bus lines
-  vector<int> line[MAX_LINES]; // stops for each line
+  int n;                         // bus lines
+  vector<int> line[MAX_LINES];   // stops for each line
   long long int news[MAX_LINES]; // news or meet for each line
-  int pos[MAX_LINES]; // current pos of bus
-  
+  int pos[MAX_LINES];            // current pos of bus
 
-  while (cin>> n && n) {
-    for (int i=0; i<n; i++){
+  while (cin >> n && n) {
+    for (int i = 0; i < n; i++) {
       line[i].clear();
       news[i] = (1LL << i);
       pos[i] = 0;
 
       int numStops;
-      for (int j=0; j < numStops; j++){
+      cin >> numStops;
+      for (int j = 0; j < numStops; j++) {
         int stop;
         cin >> stop;
         line[i].push_back(stop);
       }
-
     }
     int time = 0;
-    int maxIterations = n*n +1;
+    int maxIterations = n * n + 1;
     int lastExchangeTime = 0;
 
-    while( maxIterations > 0){
+    while (maxIterations > 0) {
       bool exchangeOccured = false;
 
-      for ( int i= 0; i<n; i++){
-        for (int j= i+1; j<n; j++){
-          if (line[i][pos[i]] == line[j][pos[j]] && (news[i] ^ news[j])){
-            maxIterations = n*n+1;
+      for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+          if (line[i][pos[i]] == line[j][pos[j]] && (news[i] ^ news[j])) {
+            maxIterations = n * n + 1;
             news[i] |= news[j];
             news[j] |= news[i];
             exchangeOccured = true;
-
           }
         }
       }
-      if (exchangeOccured){
+      if (exchangeOccured) {
         lastExchangeTime = time;
       }
-      for (int i=0; i <n; i++){
-        pos[i] = (pos[i]+1)% line[i].size();
+      for (int i = 0; i < n; i++) {
+        pos[i] = (pos[i] + 1) % line[i].size();
       }
       maxIterations--;
       time++;
-
     }
     bool allDriversKnow = true;
-    for (int i = 0; i <n; i++){
-      if (news[i] !=((1LL <<n) -1)){
+    for (int i = 0; i < n; i++) {
+      if (news[i] != ((1LL << n) - 1)) {
         allDriversKnow = false;
         break;
       }
     }
 
-    if (allDriversKnow){
+    if (allDriversKnow) {
       cout << lastExchangeTime << endl;
-    } else{
+    } else {
       cout << "NEVER" << '\n';
     }
-
   }
-    
-     return 0;
+
+  return 0;
 }
