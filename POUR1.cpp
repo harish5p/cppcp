6043a80159486cd9a63e966b63d0d85a98e91d54
Harish Pentapalli <harishpentapalli@yahoo.com>
2023-11-05 09:34:57 +0000

    POUR1

---
+++ POUR1.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int x,int y){
+    if(x==0){
+        return y;
+    }
+    return gcd(y%x,x);
+}
+
+int main(){
+    int T,a,b,c;
+    cin >> T;
+    while(T--){
+        cin >> a>>b>>c;
+        if(c%gcd(a,b)|| c>max(a,b)){
+            puts("-1");
+        }else{
+            int best = 2e9;
+            int A=0;
+            int B=0;
+            int moves=0;
+            while(A!=c&&B!=c){
+                moves++;
+                if(A==0){
+                    A=a;
+                }else if(B==b){
+                    B=0;
+                }else{
+                    int trans=min(A,b-B);
+                    A-=trans;
+                    B+=trans;
+                }
+            }
+            best=min(best,moves);
+            A=0;
+            B=0;
+            moves=0;
+            while(A!=c&&B!=c){
+                moves++;
+                if(B==0){
+                    B=b;
+                }else if(A==a){
+                    A=0;
+                }else{
+                    int trans=min(B,a-A);
+                    B-=trans;
+                    A+=trans;
+                }
+            }
+            best=min(best,moves);
+            cout << best <<'\n';
+        }
+    }
+}
